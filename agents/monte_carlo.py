0d47b39d054d77bcafee6d315b347656d42dbdc1
TryingOutSomething <jan.owyeong123@gmail.com>
2020-10-06 22:52:10 +0800

    backup of failed monte carlo algo

--- agents/monte_carlo.py
+++ agents/monte_carlo.py
@@ -7,28 +7,22 @@ ALL_POSSIBLE_ACTIONS = ['left', 'right', 'forward', 'backward', 'up', 'down']
 dimension = 4
 
 
-def gen_policy():
-    policy = {}
-
-    for i in range(dimension):
-        for j in range(dimension):
-            policy[(i, j)] = choice(ALL_POSSIBLE_ACTIONS)
-
-    return policy
-
-
 class MonteCarloAgent:
     def __init__(self):
         self.action_space = ALL_POSSIBLE_ACTIONS
-        # TODO: Change Q to a dict with all states as key and dictionary with value as each action as key and 0 as value
-        # Example
-        # {(0, 1): {'L': 0, 'D': 0, 'R': 0, 'U': 0}, (1, 2): {'L': 0, 'D': 0, 'R': 0, 'U': 0},
-        #  (0, 0): {'L': 0, 'D': 0, 'R': 0, 'U': 0}, (2, 3): {'L': 0, 'D': 0, 'R': 0, 'U': 0},
-        #  (2, 0): {'L': 0, 'D': 0, 'R': 0, 'U': 0}, (1, 0): {'L': 0, 'D': 0, 'R': 0, 'U': 0},
-        #  (2, 2): {'L': 0, 'D': 0, 'R': 0, 'U': 0}, (0, 2): {'L': 0, 'D': 0, 'R': 0, 'U': 0},
-        #  (2, 1): {'L': 0, 'D': 0, 'R': 0, 'U': 0}}
         self.Q = defaultdict(lambda: np.zeros(len(self.action_space)))
         self.returns = defaultdict(list)
+        self.policy = self.generate_random_policy()
+
+    def generate_random_policy(self):
+        policy = {}
+
+        for i in range(dimension):
+            for j in range(dimension):
+                for k in range(dimension):
+                    policy[(i, j, k)] = choice(self.action_space)
+
+        return policy
 
     def take_action_using_policy(self):
         # Returns a random action using epsilon-soft policy
